af2f52746ffa2d6215777316ee39ed9514e3a7de
dharun7405 <dharunsudha74@gmail.com>
2025-05-17 21:56:52 +0530

    Images Added

--- PacMan.java
+++ PacMan.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class PacMan extends JPanel {
     final int rowCount = 21;
@@ -21,5 +22,15 @@ public class PacMan extends JPanel {
     PacMan(){
         setPreferredSize(new Dimension(boardWidth,boardHeight));
         setBackground(Color.black);
+
+        wallImage        = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/wall.png"))).getImage();
+        blueGhostImage   = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/blueGhost.png"))).getImage();
+        orangeGhostImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/orangeGhost.png"))).getImage();
+        redGhostImage    = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/redGhost.png"))).getImage();
+        pinkGhostImage   = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/pinkGhost.png"))).getImage();
+        pacmanUpImage    = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/pacmanUp.png"))).getImage();
+        pacmanDownImage  = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/pacmanDown.png"))).getImage();
+        pacmanLeftImage  = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/pacmanLeft.png"))).getImage();
+        pacmanRightImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("./img/pacmanRight.png"))).getImage();
     }
 }
